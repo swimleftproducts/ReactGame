0f00035e83540747bf4aa4ffcb4508fb7a61db54
Mat C <57735350+m4th3w561@users.noreply.github.com>
2021-08-17 17:51:08 +0900

    fixed nav-bar

--- reactgame/src/components/Nav.js
+++ reactgame/src/components/Nav.js
@@ -21,17 +21,17 @@ const Nav = (props) => {
         )
     })
     
-    // this part returns the background menu tabl hard coded and then the results of the renderDropdowns from up above
+    // this part returns the background menu tab hard coded and then the results of the renderDropdowns from up above
     return (
         <div >
-            <nav className = "navbar position-fixed">
+            <nav className = "navbar">
                 <ul className = "nav nav-tabs ">
                     <BasicTab selectedMenu = {selectedMenu} setUserPage = {setUserPage} label = {"Background"} value = {"Background"} />
                     {/* This is used to show all the drop down menus */}
                     {renderDropdowns}
                     <BasicTab selectedMenu = {selectedMenu} setUserPage = {setUserPage} label = {"Finalize"} value = {"Finalize"} />
                     <BasicTab selectedMenu = {selectedMenu} setUserPage = {setUserPage} label = {"Results"} value = {"Results"} />
-                    <button className="btn btn-outline-danger  "><a href = "http://localhost:3000/">RESET</a></button>
+                    <button className="btn btn-outline-danger  ms-auto" ><a className=""href = "http://localhost:3000/">RESET</a></button>
                 </ul>
                 
                 
