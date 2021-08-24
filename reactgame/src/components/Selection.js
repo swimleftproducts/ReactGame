0c22eab2736dd57e19ef81125c6c16a788827901
Mat C <57735350+m4th3w561@users.noreply.github.com>
2021-08-24 18:51:30 +0900

    fixed the weird spacing in Selection.js

--- reactgame/src/components/Selection.js
+++ reactgame/src/components/Selection.js
@@ -13,13 +13,13 @@ class Selection extends Component {
                 {/* trying to layout using bootstrap for the content... */}
                 <div className="selection-btn">
                     <div className="page-btn btn-L" onClick={()=>{console.log(this.props.userChoice)}} >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="rgb(167, 7, 216)" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
   <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
 </svg>
                         {/* <button onClick={this.onClickChange}></button> */}
                     </div>
                     <div className="page-btn btn-R" onClick={()=>{}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" fill="rgb(167, 7, 216)" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
   <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
 </svg>
                         {/* <button onClick={this.onClickChange}></button> */}
