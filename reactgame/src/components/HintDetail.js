7a4ce548eb08d855987bfdaa7d8d9e583f9ee5fc
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2021-08-28 11:02:23 +0900

    fifth commit

--- reactgame/src/components/HintDetail.js
+++ reactgame/src/components/HintDetail.js
@@ -4,27 +4,19 @@ const HintDetail = ({detail, setDetail,title,content}) => {
     
     return (
         <div className="container-fluid  gx-5">
-             <div onClick={() => {   setDetail("")}}
-             className=" px-4 container-fluid" style={{"border":"grey solid 5px"}}>
-            
-            <div className="gx-1 gy-2 row align-items-center justify-content-evenly "
-            style={{"height":"350px"}}
-            >
-                <div className=" text-center col-5">
-                    <img src="/assets/2/nazgul.jpeg"
-                        style={{"height":"250px"}}
-                        alt={detail}
-                    />
-                </div>
-                <div className="text-center col-4">
-                    <h5>{title}</h5>
-                    <p>
-                        {content}
-                    </p>
+            <div onClick={() => {setDetail("")}} className="row justify-content-center align-items-center" style={{"border":"grey solid 5px"}}>
+                <div className="row align-items-center justify-content-center">
+                    <div className="col-12 col-md-4 .h-centering"  style={{"padding":"1rem"}}>
+                        <img className="justify-content-center" style={{"height":"300px"}} src="/assets/2/nazgul.jpeg" alt={detail}/>
+                    </div>
+                    <div className=" row text-center col-12 col-md-8">
+                        <h5>{title}</h5>
+                        <p style={{"width": "100%"}}>
+                            {content}
+                        </p>
+                    </div>
                 </div>
             </div>
-           
-        </div>
         </div>
        
       
