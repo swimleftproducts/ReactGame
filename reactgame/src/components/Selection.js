fdd6f3b215d9b699e3e87d391dd3858ba380a8ae
Mathew_C <57735350+m4th3w561@users.noreply.github.com>
2021-09-24 20:49:37 +0900

    wrong merge

--- reactgame/src/components/Selection.js
+++ reactgame/src/components/Selection.js
@@ -9,25 +9,31 @@ class Selection extends Component {
         return (
             <div className="selection-page container-fluid row flex-row-reverse flex-j-a">
                 <div className="current-selection col-xl-3 col-lg-12 flex-xl-column flex-lg-row flex-md-row ">
-                    <div class="card selected-card col-xl-6 col-lg-3 col-sm-3">
-                        <img src={this.props.userChoice[0].imgSrc} alt="" class="card-img-top"/>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Weapon</h5>
-                            <p class="card-text text-center">Click here to reselect</p>
+                    <div className="card selected-card col-xl-6 col-lg-3 col-sm-3">
+                        <div className="selected-card-img card-img-top">
+                            <img src={this.props.userChoice[0].imgSrc} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Weapon</h5>
+                            <p className="card-text text-center">Click here to reselect</p>
                         </div>
                     </div>
-                    <div class="card selected-card col-xl-6 col-lg-3 col-sm-3">
-                        <img src={this.props.userChoice[1].imgSrc} alt="" class="card-img-top"/>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Name</h5>
-                            <p class="card-text text-center">Click here to reselect</p>
+                    <div className="card selected-card col-xl-6 col-lg-3 col-sm-3">
+                        <div className="selected-card-img card-img-top">
+                            <img  src={this.props.userChoice[1].imgSrc} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Name</h5>
+                            <p className="card-text text-center">Click here to reselect</p>
                         </div>
                     </div>
-                    <div class="card selected-card col-xl-6 col-lg-3 col-sm-3">
-                        <img src={this.props.userChoice[2].imgSrc} alt="" class="card-img-top"/>
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Location</h5>
-                            <p class="card-text text-center">Click here to reselect</p>
+                    <div className="card selected-card col-xl-6 col-lg-3 col-sm-3">
+                        <div className="selected-card-img card-img-top">
+                            <img  src={this.props.userChoice[2].imgSrc} alt="" />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Location</h5>
+                            <p className="card-text text-center">Click here to reselect</p>
                         </div>
                     </div>
                 </div>
